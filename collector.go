49a6bc35e84a532148c3efde5c3de39343b3de26
Marcelo Mendonça <mendonca2709@gmail.com>
2022-02-26 18:35:26 -0300

    Add MVP

---
+++ collector.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/zap"
+)
+
+var (
+	up = prometheus.NewDesc(
+		"queue_it_up",
+		"Was talking to Queue-it successful.",
+		nil, nil,
+	)
+	duration = prometheus.NewDesc(
+		"queue_it_collector_collect_duration_seconds",
+		"Was talking to Queue-it successful.",
+		nil, nil,
+	)
+)
+
+type collector struct {
+	logger     *zap.Logger
+	queueitAPI *queueitAPI
+}
+
+// newCollector returns a queueitAPI connector
+func newCollector(logger *zap.Logger, api *queueitAPI) *collector {
+	logger.Debug("newCollector()")
+	c := &collector{
+		logger:     logger,
+		queueitAPI: api,
+	}
+
+	return c
+}
+
+// Describe implements Collector
+func (c *collector) Describe(ch chan<- *prometheus.Desc) {
+	c.logger.Debug("collector.Describe")
+	// metrics don't change, we can use this helper method
+	prometheus.DescribeByCollect(c, ch)
+}
+
+// Collect implements Collector
+func (c *collector) Collect(ch chan<- prometheus.Metric) {
+	c.logger.Debug("collector.Collect()")
+
+	start := time.Now()
+	defer func() {
+		// track request duration
+		now := time.Now()
+		ch <- prometheus.MustNewConstMetric(
+			duration,
+			prometheus.CounterValue,
+			float64(now.Sub(start).Seconds()),
+		)
+	}()
+
+	// Get metrics
+	metrics, err := c.queueitAPI.getMetrics()
+	if err != nil {
+		c.logger.Error("error", zap.Error(err))
+		// Queue-it api is unreachable
+		ch <- prometheus.MustNewConstMetric(up, prometheus.GaugeValue, 0)
+		return
+	}
+
+	// Contacted Queue-it api successfully
+	ch <- prometheus.MustNewConstMetric(up, prometheus.GaugeValue, 1)
+
+	// Send gauges
+	for _, m := range metrics.gauges {
+		ch <- prometheus.MustNewConstMetric(
+			prometheus.NewDesc(
+				m.exportedMetricName,
+				m.description,
+				[]string{"waiting_room_id"},
+				nil,
+			),
+			prometheus.GaugeValue,
+			m.value,
+			m.waitingRoomID,
+		)
+	}
+
+	c.logger.Debug("collector.Collect(): Finished collecting")
+}
